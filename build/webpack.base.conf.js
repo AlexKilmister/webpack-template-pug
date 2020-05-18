bd547b05904fb63dde462b1afe518830b7444b11
Alex <warmetal99@gmail.com>
2020-05-18 12:18:32 +0300

    fix static

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -74,7 +74,7 @@ module.exports = {
       test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
       loader: 'file-loader',
       options: {
-        name: '[name].[ext]'
+        name: `${PATHS.assets}fonts/[name].[ext]`
       }
     }, {
       test: /\.(png|jpe?g|gif|svg|ico)$/,
@@ -127,7 +127,7 @@ module.exports = {
     }),
     new CopyWebpackPlugin([
       { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+      //{ from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
       { from: `${PATHS.src}/static`, to: '' },
     ]),
     new SVGSpritemapPlugin(`${PATHS.src}/${PATHS.assets}img/icons-svg/**/*.svg`, {
